e8653de4e2169d46f9405bd090b3bbaf7a66b8db
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-18 17:31:31 +0100

    add sample user to be used

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,20 @@ import (
 var (
 	router = gin.Default()
 )
+
+type User struct {
+	ID uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Creating a sample user for use
+var user = User{
+	ID: 1,
+	Username: "username",
+	Password: "password",
+}
+
 
 func main() {
 	router.POST("/login", Login)
