003dadab38630b546309689f2f9c5b19652c73e2
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-18 17:42:21 +0100

    implement the Login handler function

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
 	"encoding/json"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,6 +25,28 @@ var user = User{
 	Password: "password",
 }
 
+
+func Login(c *gin.Context) {
+	var u User
+	// Get JSON body and store in u
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+
+	// Compare the user from the request with the one we defined
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+
+	token, err := CreateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
 
 func main() {
 	router.POST("/login", Login)
