e05ab9ee8c4135ba66f54d49ffebdf351c5f46fc
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-18 20:19:24 +0100

    refining the CreateToken function to create both an Access Token and a Refresh Token

--- main.go
+++ main.go
@@ -24,6 +24,16 @@ type User struct {
 	Phone string `json:"phone"`
 }
 
+type TokenDetails struct {
+	AccessToken string
+	RefreshToken string
+	AccessUuid string
+	RefreshUuid string
+	AtExpires int64
+	RtExpires int64
+}
+
+
 // Creating a sample user for use
 var user = User{
 	ID: 1,
@@ -55,22 +65,41 @@ func Login(c *gin.Context) {
 	c.JSON(http.StatusOK, token)
 }
 
-func CreateToken(userid uint64) (string, error) {
+func CreateToken(userid uint64) (*TokenDetails, error) {
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUuid = uuid.NewV4().String()
+
 	var err error
-	// Set claims and secret
+	// Create the Access Token
 	os.Setenv("ACCESS_SECRET", "my_super_secret")	// this should be in an env file
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
 	atClaims["user_id"] = userid
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
-
-	// Create the Access Token
+	atClaims["exp"] = td.AtExpires	
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return token, nil
+
+	// Creating the Refresh Token
+	os.Setenv("REFRESH_SECRET", "my_super_refresh_secret")	// this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+
+	return td, nil
 }
 
 
