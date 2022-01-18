6b7041944603e51c8409725b31db8ef34c60e8bc
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-18 18:01:38 +0100

    implement the CreateToken function

--- main.go
+++ main.go
@@ -52,7 +52,21 @@ func Login(c *gin.Context) {
 }
 
 func CreateToken(userid uint64) (string, error) {
-	
+	var err error
+	// Set claims and secret
+	os.Setenv("ACCESS_SECRET", "my_super_secret")	// this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+
+	// Create the Access Token
+	at := jwt.NewWithClaims(jwt.SigningMethodES256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 
