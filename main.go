b0f40cd105b0d9ba635c34b83974004a14ee7b67
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-19 13:05:31 +0100

    implement the refresh token route and handler: which create a new pair of access and refresh tokens when the access token expires so as not to stress the user to re-login

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func main() {
 	router.POST("/login", Login)
 	router.POST("/todo", TokenAuthMiddleware(), CreateTodo)
 	router.POST("/logout", TokenAuthMiddleware(), Logout)
+	router.POST("/token/refresh", Refresh)
 
 	log.Fatal(router.Run(":8080"))
 }
@@ -132,6 +133,75 @@ func Logout(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, "Sucessfully logged out")
+}
+
+func Refresh(c *gin.Context) {
+	mapToken := map[string]string{}
+	if err := c.ShouldBindJSON(&mapToken); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	refreshToken := mapToken["refresh_token"]
+
+	// Verify the token
+	os.Setenv("REFRESH_SECRET", "my_super_refresh_secret") // should be in an env file
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		// make sure that the token method conforms to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("REFRESH_SECRET")), nil
+	})
+	// if there is an error, the token must have expired
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "Refresh token expired")
+		return
+	}
+	// is token valid?
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+	// Since token is valid, get the uuid
+	claims, ok := token.Claims.(jwt.MapClaims) // the token claims should conform to MapClaims
+	if ok && token.Valid {
+		refreshUuid, ok := claims["refresh_uuid"].(string) // convert interface to string
+		if !ok {
+			c.JSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+			return
+		}
+		// Delete the previous Refresh Token
+		deleted, delErr := DeleteAuth(refreshUuid)
+		if delErr != nil || deleted == 0 {
+			c.JSON(http.StatusUnauthorized, "unauthorized")
+			return
+		}
+
+		// Create new pairs of refresh and access tokens
+		ts, createErr := CreateToken(userId)
+		if createErr != nil {
+			c.JSON(http.StatusForbidden, createErr.Error())
+			return
+		}
+		// Save the tokens metadat to Redis
+		saveErr := CreateAuth(userId, ts)
+		if saveErr != nil {
+			c.JSON(http.StatusForbidden, saveErr.Error())
+			return
+		}
+		tokens := map[string]string{
+			"access_token": ts.AccessToken,
+			"refresh_token": ts.RefreshToken
+		}
+		c.JSON(http.StatusCreated, tokens)
+	} else {
+		c.JSON(http.StatusUnauthorized, "refresh expired")
+	}
 }
 
 
